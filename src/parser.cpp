4649f06190e6c521fb8f6f0ea2a5fab85b8941f8
ssmingz <49485416+ssmingz@users.noreply.github.com>
2020-06-03 14:47:04 +0800

    Fix parser.cpp for TAC-generation

--- src/parser.cpp
+++ src/parser.cpp
@@ -10,6 +10,8 @@ using namespace std;
 
 Scanner::Token token; // current token
 SymbolTable symTable;
+TAC tac;
+string midresult;
 
 Parser::Parser(const string& filename) {
 	this->ParserFilename = filename;
@@ -37,9 +39,9 @@ Parser::TreeNode* Parser::parseProgram() {
 		t->child[0] = parseConstList();
 		token = scanner.nextToken();
 	}
-	//±äÁ¿¶¨ÒåºÍÓÐ·µ»ØÖµµÄº¯ÊýÓÐÏàÍ¬µÄÉùÃ÷Í·²¿£¬ÐèÔ¤¶Á3¸öµ¥´Ê
+	//å˜é‡å®šä¹‰å’Œæœ‰è¿”å›žå€¼çš„å‡½æ•°æœ‰ç›¸åŒçš„å£°æ˜Žå¤´éƒ¨ï¼Œéœ€é¢„è¯»3ä¸ªå•è¯
 	while (token.lexeme == "int" || token.lexeme == "char") {
-		Scanner::Token tmp1, tmp2; //ÓÃÓÚ»Ö¸´ÏÖ³¡
+		Scanner::Token tmp1, tmp2; //ç”¨äºŽæ¢å¤çŽ°åœº
 		tmp1 = token;
 		token = scanner.nextToken();
 		if (token.type != Scanner::TokenType::ID) {
@@ -48,7 +50,7 @@ Parser::TreeNode* Parser::parseProgram() {
 		}
 		tmp2 = token;
 		token = scanner.nextToken();
-		//ÈôÎª,»ò[»ò£»¾ÍÊÇ±äÁ¿¶¨Òå
+		//è‹¥ä¸º,æˆ–[æˆ–ï¼›å°±æ˜¯å˜é‡å®šä¹‰
 		if (token.lexeme == "," || token.lexeme == "[" || token.lexeme == ";") {
 			scanner.ungetToken(token);
 			scanner.ungetToken(tmp1);
@@ -69,7 +71,7 @@ Parser::TreeNode* Parser::parseProgram() {
 		}
 		token = scanner.nextToken();
 	}
-	//º¯Êý²¿·Ö,ÕâÀïtoken=int/char/void
+	//å‡½æ•°éƒ¨åˆ†,è¿™é‡Œtoken=int/char/void
 	while (token.lexeme == "int" || token.lexeme == "char" || token.lexeme == "void") {
 		Scanner::Token tmp1 = token;
 		token = scanner.nextToken();
@@ -157,6 +159,9 @@ Parser::TreeNode* Parser::parseConstDec() {
 				return t;
 			}
 		}
+
+		//generate tac
+		tac.NewQuad( (flag==0)?(TAC::QuadOpKind::CSTI):(TAC::QuadOpKind::CSTC), p->token.lexeme, p->child[0]->token.lexeme, "");
 	}
 	scanner.ungetToken(token);
 	return t;
@@ -240,11 +245,14 @@ Parser::TreeNode* Parser::parseVarDec() {
 		}
 
 		//symbol table action
+		//generate tac
 		if (p->nodeType == Parser::NodeType::N_ARRAY) {
 			symTable.define(p->token.lexeme, t->child[0]->token.lexeme, SymbolTable::SymbolKind::VAR, 0, atoi((p->child[0]->token.lexeme).c_str()));
+			tac.NewQuad( (t->child[0]->token.lexeme == "int")?(TAC::QuadOpKind::ARRI):(TAC::QuadOpKind::ARRC), p->token.lexeme, (p->child[0]->token.lexeme).c_str(), "");
 		}
 		else {
 			symTable.define(p->token.lexeme, t->child[0]->token.lexeme, SymbolTable::SymbolKind::VAR, 0, (int)-1);
+			tac.NewQuad( (t->child[0]->token.lexeme == "int")?(TAC::QuadOpKind::INT):(TAC::QuadOpKind::CHAR), p->token.lexeme, "", "");
 		}
 
 		t->child.push_back(p);
@@ -324,6 +332,9 @@ Parser::TreeNode* Parser::parseParameterList() {
 		//symbol table action
 		symTable.define(p1->child[1]->token.lexeme, p1->child[0]->token.lexeme, SymbolTable::SymbolKind::ARG, 0, (int)-1);
 
+		//generate tac
+		tac.NewQuad(TAC::QuadOpKind::PARM, p1->child[1]->token.lexeme, p1->child[0]->token.lexeme, "");
+
 		if (t->child[0] == nullptr) {
 			t->child[0] = new Parser::TreeNode;
 			t->child[0] = p1;
@@ -364,6 +375,9 @@ Parser::TreeNode* Parser::parseFunctionDef() {
 	t->child[1]->nodeType = Parser::NodeType::N_FUNC_NAME;
 	t->child[1]->token = token;
 
+	//generate tac
+	tac.NewQuad(TAC::QuadOpKind::FUNC, token.lexeme, t->child[0]->token.lexeme, "");
+
 	//symbol table action
 	symTable.define(t->child[1]->token.lexeme, t->child[0]->token.lexeme, SymbolTable::SymbolKind::FUNCTION, 0, (int)-1);
 
@@ -377,6 +391,9 @@ Parser::TreeNode* Parser::parseFunctionDef() {
 	if (!match("{")) { return t; }
 	t->child.push_back(parseCompoundStatement());
 	if (!match("}")) { return t; }
+
+	//generate tac
+	tac.NewQuad(TAC::QuadOpKind::END, "", "", "");
 
 	//symbol table action
 	symTable.localScopeTables.insert({ t->child[1]->token.lexeme, symTable.localScopeTable });
@@ -393,12 +410,18 @@ Parser::TreeNode* Parser::parseMainFunction() {
 		!match("(") || !match(")") || !match("{"))
 		return t;
 
+	//generate tac
+	tac.NewQuad(TAC::QuadOpKind::FUNC, "main", "void", "");
+
 	//symbol table action
 	symTable.define("main", "void", SymbolTable::SymbolKind::FUNCTION, 0, (int)-1);
 
 	t->child[0] = new Parser::TreeNode;
 	t->child[0] = parseCompoundStatement();
 	if (!match("}")) return t;
+
+	//generate tac
+	tac.NewQuad(TAC::QuadOpKind::END, "", "", "");
 
 	//symbol table action
 	symTable.localScopeTables.insert({ "main", symTable.localScopeTable });
@@ -525,7 +548,7 @@ Parser::TreeNode* Parser::parseStatement() {
 		}
 		return t;
 	}
-	// ¿ÕÓï¾ä
+	// ç©ºè¯­å¥
 	else if (token.lexeme == ";") {
 		t->nodeType = Parser::NodeType::N_NONE;
 		scanner.ungetToken(token);
@@ -544,6 +567,7 @@ Parser::TreeNode* Parser::parseStatement() {
 }
 
 Parser::TreeNode* Parser::parseReturnStmt() {
+	string midvar;
 	Parser::TreeNode* t = nullptr;
 	token = scanner.nextToken();
 	if (token.lexeme != "return") {
@@ -556,20 +580,27 @@ Parser::TreeNode* Parser::parseReturnStmt() {
 	token = scanner.nextToken();
 	if (token.lexeme == "(") {
 		t = parseExpression();
+		midvar = midresult;
 		p->child[0] = t;
 		token = scanner.nextToken();
 		if (token.lexeme != ")") {
 			syntaxError(")", token);
 			return t;
 		}
+		//generate tac
+		tac.NewQuad(TAC::QuadOpKind::RET, "", "", midvar);
 	}
-	else
+	else {
 		scanner.ungetToken(token);
+		//generate tac
+		tac.NewQuad(TAC::QuadOpKind::RET, "", "", "");
+	}
 	t = p;
 	return t;
 }
 
 Parser::TreeNode* Parser::parseReadStmt() {
+	string midname;
 	Parser::TreeNode* t = new Parser::TreeNode;
 	token = scanner.nextToken();
 	if (token.lexeme != "scanf") {
@@ -587,11 +618,15 @@ Parser::TreeNode* Parser::parseReadStmt() {
 			t->child[0] = new Parser::TreeNode;
 			t->child[0]->token = token;
 			t->child[0]->nodeType = Parser::NodeType::N_VAR;
+			midname = token.lexeme;
 
 			//check symbol table
 			SymbolTable::SymbolKind ckind = symTable.kindOf(token.lexeme);
 			if (ckind != SymbolTable::SymbolKind::VAR)
 				error(VAR_NO_DEF_ERROR, token.line);
+
+			//generate tac
+			tac.NewQuad(TAC::QuadOpKind::RD, "", "", midname);
 
 			token = scanner.nextToken();
 			while (token.lexeme == ",") {
@@ -604,11 +639,14 @@ Parser::TreeNode* Parser::parseReadStmt() {
 					Parser::TreeNode* p = new Parser::TreeNode;
 					p->token = token;
 					p->nodeType = Parser::NodeType::N_VAR;
+					midname = token.lexeme;
 
 					//check symbol table
 					ckind = symTable.kindOf(token.lexeme);
 					if (ckind != SymbolTable::SymbolKind::VAR)
 						error(VAR_NO_DEF_ERROR, token.line);
+					//generate tac
+					tac.NewQuad(TAC::QuadOpKind::RD, "", "", midname);
 
 					t->child.push_back(p);
 					token = scanner.nextToken();
@@ -622,6 +660,7 @@ Parser::TreeNode* Parser::parseReadStmt() {
 }
 
 Parser::TreeNode* Parser::parseWriteStmt() {
+	string midvar1 = "", midvar2 = "";	//1 for string, 2 for expression
 	Parser::TreeNode* t = new Parser::TreeNode;
 	token = scanner.nextToken();
 	if (token.lexeme != "printf") {
@@ -636,11 +675,13 @@ Parser::TreeNode* Parser::parseWriteStmt() {
 		token = scanner.nextToken();
 		t->child[0] = new Parser::TreeNode;
 		if (token.type == Scanner::TokenType::STRING) {
+			midvar1 = token.lexeme;
 			t->child[0]->token = token;
 			t->child[0]->nodeType = Parser::NodeType::N_STRING;
 			token = scanner.nextToken();
 			if (token.lexeme == ",") {
 				Parser::TreeNode* p = parseExpression();
+				midvar2 = midresult;
 				t->child.push_back(p);
 				token = scanner.nextToken();
 			}
@@ -648,30 +689,56 @@ Parser::TreeNode* Parser::parseWriteStmt() {
 		else {
 			scanner.ungetToken(token);
 			Parser::TreeNode* p = parseExpression();
+			midvar2 = midresult;
 			t->child[0] = p;
 			token = scanner.nextToken();
 		}
 		if (token.lexeme != ")") syntaxError(")", token);
 	}
+	//generate tac
+	tac.NewQuad(TAC::QuadOpKind::WR, midvar1, midvar2, "");
 	return t;
 }
 
 Parser::TreeNode* Parser::parseCondition() {
+	string midvar1, midvar2;
 	Parser::TreeNode* t = new Parser::TreeNode;
 	t->nodeType = Parser::NodeType::N_CONDITION;
 	Parser::TreeNode* p1 = parseExpression();
+	midvar1 = midresult;
 	token = scanner.nextToken();
-	if (token.lexeme == ">" || token.lexeme == "<" || token.lexeme == ">=" || token.lexeme == "<=" || token.lexeme == "==" || token.lexeme == "==") {
+	if (token.lexeme == ">" || token.lexeme == "<" || token.lexeme == ">=" || token.lexeme == "<=" || token.lexeme == "==" || token.lexeme == "!=") {
 		Parser::TreeNode* p2 = new Parser::TreeNode;
 		p2->nodeType = Parser::NodeType::N_RELOP;
 		p2->token = token;
 		p2->child[0] = new Parser::TreeNode;
 		p2->child[0] = p1;
 		p2->child.push_back(parseExpression());
+		midvar2 = midresult;
+		midresult = tac.NewVar();
+
+		//generate tac
+		string relop = p2->token.lexeme;
+		if (relop == ">")
+			tac.NewQuad(TAC::QuadOpKind::GTR, midvar1, midvar2, midresult);
+		else if(relop == "<")
+			tac.NewQuad(TAC::QuadOpKind::LES, midvar1, midvar2, midresult);
+		else if (relop == ">=")
+			tac.NewQuad(TAC::QuadOpKind::GEQ, midvar1, midvar2, midresult);
+		else if (relop == "<=")
+			tac.NewQuad(TAC::QuadOpKind::LEQ, midvar1, midvar2, midresult);
+		else if (relop == "==")
+			tac.NewQuad(TAC::QuadOpKind::EQU, midvar1, midvar2, midresult);
+		else if (relop == "!=")
+			tac.NewQuad(TAC::QuadOpKind::NEQ, midvar1, midvar2, midresult);
+
 		p1 = p2;
 	}
 	else if (token.lexeme == ")") {
 		scanner.ungetToken(token);
+		//generate tac
+		midresult = tac.NewVar();
+		tac.NewQuad(TAC::QuadOpKind::NEQ, midvar1, "", midresult);
 	}
 	else {
 		syntaxError("relation operator", token);
@@ -683,14 +750,25 @@ Parser::TreeNode* Parser::parseCondition() {
 }
 
 Parser::TreeNode* Parser::parseIfStmt() {
+	string label1 = tac.NewLabel(), label2 = tac.NewLabel(), midvar;
 	Parser::TreeNode* t = new Parser::TreeNode;
 	t->nodeType = Parser::NodeType::N_IF_STMT;
 	if (!match("if")) { return t; }
 	if (!match("(")) { return t; }
 	t->child[0] = new Parser::TreeNode;
 	t->child[0] = parseCondition();
+	midvar = midresult;
+	
+	//generate tac
+	tac.NewQuad(TAC::QuadOpKind::IFZ, midvar, "", label1);
+	
 	if (!match(")")) { return t; }
 	t->child.push_back(parseStatement());
+	
+	//generate tac
+	tac.NewQuad(TAC::QuadOpKind::GOTO, "", "", label2);
+	tac.NewQuad(TAC::QuadOpKind::LAB, "", "", label1);
+
 	token = scanner.nextToken();
 	if (token.lexeme == "else") {
 		t->child.push_back(parseStatement());
@@ -698,19 +776,33 @@ Parser::TreeNode* Parser::parseIfStmt() {
 	else {
 		scanner.ungetToken(token);
 	}
+	tac.NewQuad(TAC::QuadOpKind::LAB, "", "", label2);	//generate tac
 	return t;
 }
 
 Parser::TreeNode* Parser::parseLoopStmt() {
+	string label1 = tac.NewLabel(), label2 = tac.NewLabel(); //for tac generation
+	string midname1, midname2, midname3, midvar;
 	Parser::TreeNode* t = new Parser::TreeNode;
 	token = scanner.nextToken();
 	if (token.lexeme == "while") {
 		t->nodeType = Parser::NodeType::N_WHILE_STMT;
 		if (!match("(")) { return t; }
 		t->child[0] = new Parser::TreeNode;
+
+		//generate tac
+		tac.NewQuad(TAC::QuadOpKind::LAB, "", "", label1);
+
 		t->child[0] = parseCondition();
 		if (!match(")")) { return t; }
+
+		//generate tac
+		tac.NewQuad(TAC::QuadOpKind::IFZ, midresult, "", label2);
+
 		t->child.push_back(parseStatement());
+		//generate tac
+		tac.NewQuad(TAC::QuadOpKind::GOTO, "", "", label1);
+		tac.NewQuad(TAC::QuadOpKind::LAB, "", "", label2);
 	}
 	if (token.lexeme == "for") {
 		t->nodeType = Parser::NodeType::N_FOR_STMT;
@@ -734,23 +826,37 @@ Parser::TreeNode* Parser::parseLoopStmt() {
 		p1->child[0] = new Parser::TreeNode;
 		p1->child[0]->token = token;
 		p1->child[0]->nodeType = Parser::NodeType::N_VAR;
+		midname1 = token.lexeme;
 		if (!match("=")) { return t; }
 		p1->token = token;
 		p1->child.push_back(parseExpression());
+		tac.NewQuad(TAC::QuadOpKind::ASN, midresult, "", midname1);			//generate tac
 		t->child[0] = new Parser::TreeNode;
 		t->child[0] = p1;
 		if (!match(";")) { return t; }
+		tac.NewQuad(TAC::QuadOpKind::LAB, "", "", label1);					//generate tac
 		t->child.push_back(parseCondition());
+		tac.NewQuad(TAC::QuadOpKind::IFZ, midresult, "", label2);			//generate tac
 		if (!match(";")) { return t; }
 		token = scanner.nextToken();
 		if (token.type != Scanner::TokenType::ID) {
 			syntaxError("identifier", token);
 			return t;
 		}
+		
+		//check symbol table
+		ckind = symTable.kindOf(token.lexeme);
+		if (ckind == SymbolTable::SymbolKind::NONE) {
+			error(ID_NO_DEF_ERROR, token.line);
+			exit(-1);
+			return t;
+		}
+
 		Parser::TreeNode* p2 = new Parser::TreeNode;
 		p2->nodeType = Parser::NodeType::N_FOR_INCREMENT;
 		p2->child[0] = new Parser::TreeNode;
 		p2->child[0]->token = token;
+		midname2 = token.lexeme;
 		p2->child[0]->nodeType = Parser::NodeType::N_VAR;
 		if (!match("=")) { return t; }
 		p2->token = token;
@@ -759,14 +865,7 @@ Parser::TreeNode* Parser::parseLoopStmt() {
 			syntaxError("identifier", token);
 			return t;
 		}
-
-		//check symbol table
-		ckind = symTable.kindOf(token.lexeme);
-		if (ckind == SymbolTable::SymbolKind::NONE) {
-			error(ID_NO_DEF_ERROR, token.line);
-			exit(-1);
-			return t;
-		}
+		midname3 = token.lexeme;
 
 		Scanner::Token tmp = token;
 		Parser::TreeNode* p3 = new Parser::TreeNode;
@@ -795,11 +894,19 @@ Parser::TreeNode* Parser::parseLoopStmt() {
 		t->child.push_back(p2);
 		if (!match(")")) { syntaxError(")", token); return t; }
 		t->child.push_back(parseStatement());
+
+		//generate tac
+		midvar = tac.NewVar();
+		tac.NewQuad((p3->token.lexeme == "+") ? (TAC::QuadOpKind::ADD) : (TAC::QuadOpKind::SUB), midname3, p3->child[1]->token.lexeme, midvar);
+		tac.NewQuad(TAC::QuadOpKind::ASN, midvar, "", midname2);
+		tac.NewQuad(TAC::QuadOpKind::GOTO, "", "", label1);
+		tac.NewQuad(TAC::QuadOpKind::LAB, "", "", label2);
 	}
 	return t;
 }
 
 Parser::TreeNode* Parser::parseValueParameters() {
+	vector<string> midparm;
 	Parser::TreeNode* t = new Parser::TreeNode;
 	t->nodeType = Parser::NodeType::N_VPARAMS;
 	t->token.line = token.line;
@@ -815,16 +922,27 @@ Parser::TreeNode* Parser::parseValueParameters() {
 	else {
 		t->child.push_back(parseExpression());
 	}
+	midparm.push_back(midresult);
 	token = scanner.nextToken();
 	while (token.lexeme == ",") {
 		t->child.push_back(parseExpression());
+		midparm.push_back(midresult);
 		token = scanner.nextToken();
 	}
+
+	//generate tac
+	int len = midparm.size();
+	for (int i = 0; i < len; i++) {
+		tac.NewQuad(TAC::QuadOpKind::PUSH, "", "", midparm[i]);
+	}
+	midparm.~vector();
+
 	scanner.ungetToken(token);
 	return t;
 }
 
 Parser::TreeNode* Parser::parseCallStmt() {
+	string midvar, midname, ctype;
 	Parser::TreeNode* t = new Parser::TreeNode;
 	t->nodeType = Parser::NodeType::N_CALL_STMT;
 	token = scanner.nextToken();
@@ -835,15 +953,27 @@ Parser::TreeNode* Parser::parseCallStmt() {
 	t->child[0] = new Parser::TreeNode;
 	t->child[0]->token = token;
 	t->child[0]->nodeType = Parser::NodeType::N_FUNC_NAME;
+	midname = token.lexeme;
 
 	//check symbol table
 	SymbolTable::SymbolKind  ckind = symTable.kindOf(token.lexeme);
 	if (ckind != SymbolTable::SymbolKind::FUNCTION)
 		error(FUNC_NO_DEF_ERROR, token.line);
+	ctype = symTable.typeOf(token.lexeme);
 
 	if (!match("(")) { return t; }
 	t->child.push_back(parseValueParameters());
 	if (!match(")")) { return t; }
+
+	//generate tac
+	if (ctype == "void") {
+		tac.NewQuad(TAC::QuadOpKind::CALL, midname, "", "");
+	} else {
+		midvar = tac.NewVar();
+		tac.NewQuad(TAC::QuadOpKind::CALL, midname, "", midvar);
+		midresult = midvar;
+	}
+
 	return t;
 }
 
@@ -875,17 +1005,27 @@ Parser::TreeNode* Parser::parseLeftValue() {
 }
 
 Parser::TreeNode* Parser::parseAssignStmt() {
+	string index;
 	Parser::TreeNode* t = new Parser::TreeNode;
 	t->nodeType = Parser::NodeType::N_ASSIGN;
 	t->child[0] = parseLeftValue();
+	index = midresult;
 	token = scanner.nextToken();
 	t->token = token;
 	t->child.push_back(parseExpression());
+	//generate tac
+	if (t->child[0]->nodeType == Parser::NodeType::N_ARRAY) {
+		tac.NewQuad(TAC::QuadOpKind::STORE, midresult, index, t->child[0]->token.lexeme);
+	} else {
+		tac.NewQuad(TAC::QuadOpKind::ASN, midresult, "", t->child[0]->token.lexeme);
+	}
 	return t;
 }
 
 Parser::TreeNode* Parser::parseExpression() {
+	string midvar1, midvar2, midvar3;	//for tac generation
 	Parser::TreeNode* t = parseTerm();
+	midvar3 = midresult;
 	token = scanner.nextToken();
 	while (token.lexeme == "+" || token.lexeme == "-") {
 		Parser::TreeNode* p = new Parser::TreeNode;
@@ -894,15 +1034,25 @@ Parser::TreeNode* Parser::parseExpression() {
 		if (p->child[0] == nullptr) p->child[0] = t;
 		else p->child.push_back(t);
 		t = p;
+		midvar1 = midvar3;
 		p->child.push_back(parseTerm());
+
+		//generate tac
+		midvar2 = midresult;
+		midvar3 = tac.NewVar();
+		tac.NewQuad( (p->token.lexeme == "+")?(TAC::QuadOpKind::ADD):(TAC::QuadOpKind::SUB), midvar1, midvar2, midvar3 );
+
 		token = scanner.nextToken();
 	}
 	scanner.ungetToken(token);
+	midresult = midvar3;
 	return t;
 }
 
 Parser::TreeNode* Parser::parseTerm() {
+	string midvar1, midvar2, midvar3;
 	Parser::TreeNode* t = parseFactor();
+	midvar3 = midresult;
 	token = scanner.nextToken();
 	while (token.lexeme == "*" || token.lexeme == "/") {
 		Parser::TreeNode* p = new Parser::TreeNode;
@@ -911,14 +1061,23 @@ Parser::TreeNode* Parser::parseTerm() {
 		if (p->child[0] == nullptr) p->child[0] = t;
 		else p->child.push_back(t);
 		t = p;
+		midvar1 = midvar3;
 		p->child.push_back(parseFactor());
+		
+		//generate tac
+		midvar2 = midresult;
+		midvar3 = tac.NewVar();
+		tac.NewQuad( (p->token.lexeme == "*")?(TAC::QuadOpKind::MUL):(TAC::QuadOpKind::DIV), midvar1, midvar2, midvar3 );
+
 		token = scanner.nextToken();
 	}
 	scanner.ungetToken(token);
+	midresult = midvar3;
 	return t;
 }
 
 Parser::TreeNode* Parser::parseFactor() {
+	string midvar;
 	Parser::TreeNode* t = nullptr;
 	token = scanner.nextToken();
 	if (token.type == Scanner::TokenType::ERROR) {
@@ -949,11 +1108,13 @@ Parser::TreeNode* Parser::parseFactor() {
 		t = new Parser::TreeNode;
 		t->token = token;
 		t->nodeType = Parser::NodeType::N_INT;
+		midresult = t->token.lexeme;
 	}
 	else if (token.type == Scanner::TokenType::CHAR) {
 		t = new Parser::TreeNode;
 		t->token = token;
 		t->nodeType = Parser::NodeType::N_CHAR;
+		midresult = t->token.lexeme;
 	}
 	else if (token.type == Scanner::TokenType::ID) {
 		t = new Parser::TreeNode;
@@ -966,6 +1127,7 @@ Parser::TreeNode* Parser::parseFactor() {
 		if (ckind == SymbolTable::SymbolKind::NONE)
 			error(ID_NO_DEF_ERROR, token.line);
 
+		midresult = t->token.lexeme;
 		token = scanner.nextToken();
 		if (token.lexeme == "[") {
 			t->nodeType = Parser::NodeType::N_ARRAY;
@@ -976,6 +1138,7 @@ Parser::TreeNode* Parser::parseFactor() {
 				error(ARRAY_NO_DEF_ERROR, token.line);
 
 			t->child[0] = parseExpression();
+			string index = midresult;
 
 			//check symbol table
 			//to see if index of array out of boundary
@@ -992,6 +1155,11 @@ Parser::TreeNode* Parser::parseFactor() {
 				syntaxError("]", token);
 				return t;
 			}
+
+			//generate tac
+			midresult = tac.NewVar();
+			tac.NewQuad(TAC::QuadOpKind::LOAD, t->token.lexeme, index, midresult);
+
 		}
 		else if (token.lexeme == "(") {
 			scanner.ungetToken(token);
